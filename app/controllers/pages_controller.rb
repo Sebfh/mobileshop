21c18e8bd240d555f01e2fde29a076581c395f32
Sebastiaan Hoejenbos <sebastiaan.hoejenbos@gmail.com>
2012-10-04 23:26:55 +0200

    added a Pages#index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,6 @@
 
 class PagesController < ApplicationController
-  # GET /pages
-  # GET /pages.json
   def index
-    @pages = Page.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @pages }
-    end
   end
 
   # GET /pages/1
