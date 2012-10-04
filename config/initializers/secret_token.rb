1e95d02994620e9772b174967bfbbef52fd9ef03
Sebastiaan Hoejenbos <sebastiaan.hoejenbos@gmail.com>
2012-10-04 21:37:46 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobileshop::Application.config.secret_token = '6b090c9cb38e18d5ed44bf849cbad8ab94414e0df5d5bf48b5d67c6a63727fb8b216a6bec4189d636e3488f740d55e587708a41587174540c343214e801b411b'
